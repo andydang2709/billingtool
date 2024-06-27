fc3d410a1a27cd14681fa0519e4b4eda5b0660af
andydang <andykhangdang@gmail.com>
2024-06-27 14:23:55 -0400

    first commit

---
+++ billingtool.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+df = pd.read_csv("21cl.csv")
+
+def clean_data(df):
+    # Drop rows with missing data in column: 'First Name'
+    df = df.dropna(subset=['First Name'])
+    # Select columns: 'First Name', 'Last Name' and 3 other columns
+    df = df.loc[:, ['First Name', 'Last Name', 'Check In', 'Check Out', 'Room Type']]
+    # Change column type to datetime64[ns] for column: 'Check In'
+    df = df.astype({'Check In': 'datetime64[ns]'})
+    # Change column type to datetime64[ns] for column: 'Check Out'
+    df = df.astype({'Check Out': 'datetime64[ns]'})
+    df = df.reset_index(drop=True)
+    return df
+
+df_clean = clean_data(df.copy())
+
+df_clean['Days Stayed'] = (df_clean['Check Out']-df_clean['Check In']).dt.days
+
+single_price = int(input("Input Single Room's Price"))
+double_price = int(input("Input Double Room's Price"))
+
+df_clean['Unit Price'] = df_clean['Room Type'].apply(lambda x: single_price if x == 'Single' else double_price if x == 'Double' else None)
+
+df_clean['Charge'] = df_clean['Days Stayed'] * df_clean['Unit Price']
+
+total_charge_per_group = df_clean.groupby(['Check In', 'Check Out']).agg(
+    {'Charge': 'sum', 'Days Stayed': 'sum', 'Unit Price': 'first'}
+).reset_index()
+
+total_charge_per_group
